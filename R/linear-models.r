00ae41c63ebef62cd54690759344ce80516d35a2
Chang <c.su@yale.edu>
2018-09-14 14:25:34 -0400

    9.14 add QR, which fail withe patho data

--- R/linear-models.r
+++ R/linear-models.r
@@ -1,15 +1,25 @@
 
 #' Fit a linear model
 #'
-#' @description This function passes parameters to the lm function.
-#' @param formula a formula
+#' @description This function use QR decomposition to solve OLS.
+#' @param formula formula of the regression model
 #' @param data a data.frame
 #' @return An lm object
 #' @importFrom stats lm
+#' @importFrom stats model.matrix
 #' @examples
 #' fit <- linear_model(Sepal.Length ~., iris)
 #' summary(fit)
 #' @export
 linear_model <- function(formula, data) {
-  lm(formula, data)
+  X_design <- model.matrix(formula, data)
+  qr_object = qr(X_design) 
+  Q <- qr.Q(qr_object)
+  R <- qr.R(qr_object)
+  y <- unlist(data[all.vars(formula)[1]])
+  Qty <- crossprod(Q, y)
+  beta_hat <- backsolve(R, Qty)
+  lm_list <- lm(formula,data)
+  lm_list$coefficients <- beta_hat
+  return (lm_list)
 }
