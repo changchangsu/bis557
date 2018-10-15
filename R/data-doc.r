c3fbe0acd6c67cd19c1b3b143eec594545604bef
Chang <c.su@yale.edu>
2018-10-14 22:25:28 -0400

    homework 2

--- R/data-doc.r
+++ R/data-doc.r
@@ -8,3 +8,22 @@
 #' on linear regressions. This data set gives us a basis for exploration.
 NULL
 
+#' Training data for the 2nd homework vignette
+#'
+#' @name ridge_train
+#' @docType data
+#' @rdname ridge_train
+#' @description In homework 2 we build a ridge regression model. 
+#' For illustration, we would build ridge regression using this data set 
+#' and validate its prediction performance on another data set, ridge_test
+"ridge_train"
+
+#' Validation data for the 2nd homework vignette
+#' 
+#' @name ridge_test
+#' @docType data
+#' @rdname ridge_test
+#' @description In homework 2 we build a ridge regression model.
+#' For illustration, we would use this data set to assess the prediction error of 
+#' the ridge regreession model built on training data.
+"ridge_test"
---
+++ R/ridge_reg.r
@@ -0,0 +1,53 @@
+#' Fit a Ridge Regression model with given lambda
+#'
+#' @description This function use SVD decomposition to solve ridge regression.
+#' @param formula formula of the regression model, where intercept term is dropped
+#' @param data a data.frame, with each column scaled to have mean 0 and unit vairance
+#' @param lambda tuning parameter of ridge regression, controlling panalty
+#' @return An Ridge object
+#' @importFrom stats model.matrix
+#' @examples
+#' fit <- ridge_reg(y ~.-1, ridge_train, 0.01)
+#' summary(fit)
+#' @export
+#' 
+
+ridge_reg <- function(form, lambda, d){
+  
+  rownames(d) <- NULL 
+  # m <- model.matrix(form, d)
+  m <- model.matrix(form, d)
+  y <- matrix(d[,as.character(form)[2]], ncol=1)
+  y <- y[as.numeric(rownames(m)), , drop=F] 
+  n <- nrow(m)
+  p<- ncol(m)
+  
+  # # Remove the intercept by centering variables
+  # mean_x <- colMeans(m)
+  # mean_y <- mean(y)
+  # m <- m - matrix(rep(mean_x, n), nrow = n, byrow = T)
+  # y <- y - mean_y
+  # 
+  # # Rescale the predictors to make the penalty 'fair'
+  # scaling <- sqrt(colMeans(m^2))
+  # m <- m / matrix(rep(scaling, n), nrow = n, byrow = T)
+  
+  # Fit via svd
+  svd_obj <- svd(m)
+  U <- svd_obj$u
+  V <- svd_obj$v
+  svals <- svd_obj$d
+  
+  D <- diag(svals / (svals^2 + lambda))
+  beta <- V %*% D %*% t(U) %*% y
+  rownames(beta) <- colnames(m)
+  
+  # # Transform the beta back to the orignial scale of the data
+  # beta <- beta / scaling
+  # beta <- rbind(mean_y - beta * matrix(mean_x, ncol=1), beta)
+  # rownames(beta)[1] <- 'Intercept'
+  
+  ret <- list(coef = beta, lambda = lambda)
+  class(ret) <- "ridge_reg"
+  ret
+}
